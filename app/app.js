3d41a3f4dc3dc88836b58d36e7ce15e7b20aeb3a
Jawez <Jawez0@126.com>
2023-10-17 21:59:04 +0800

    feat: 静态资源路径调整为/，运行提示首页路径调整为/index

--- app/app.js
+++ app/app.js
@@ -11,13 +11,13 @@ import WebSocket from './extension/socket/socket';
  * 首页
  */
 function indexPage (app) {
-  app.context.loader.mainRouter.get('/', async ctx => {
+  app.context.loader.mainRouter.get('/index', async ctx => {
     ctx.type = 'html';
     ctx.body = `<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor:
       pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family:
       "Century Gothic","Microsoft yahei"; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal;
       margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"><p>
-      Lin <br/><span style="font-size:30px">心上无垢，林间有风。</span></p></div> `;
+      <span style="font-size:30px">资源管理系统</span></p></div> `;
   });
 }
 
@@ -96,7 +96,7 @@ async function createApp () {
   const app = new Koa();
   applyBodyParse(app);
   applyCors(app);
-  applyStatic(app);
+  applyStatic(app, '/');
   const { log, error, Lin, multipart } = require('lin-mizar');
   app.use(log);
   app.on('error', error);
